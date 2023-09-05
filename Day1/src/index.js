07318a97382b172b92bf44c275bde0c25e3dd585
Chaitenya-rajurkar <rajuchetan547@gmail.com>
2023-09-05 20:59:29 +0530

    component create

--- Day1/src/index.js
+++ Day1/src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from './Header';
+import Footer from './Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Header></Header>
+    <App></App>
+<Footer></Footer>
+
+
   </React.StrictMode>
 );
 
@@ -15,3 +22,24 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// real dom 
+//vitural dom 
+
+
+// there are two types of component 
+
+// 1)functional component
+// 2)class component 
+
+
+// how a funtion return html?
+
+// react js >jsx 
+
+// javascript xml(extensible markup language)  
+
+
+<input type='text' />
+
