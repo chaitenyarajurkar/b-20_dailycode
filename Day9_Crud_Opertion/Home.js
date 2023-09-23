f94fcbff45d05b4e8237a71d52148fee1ff7fffd
Chaitenya-rajurkar <rajuchetan547@gmail.com>
2023-09-23 21:42:12 +0530

    crud

---
+++ Day9_Crud_Opertion/Home.js
@@ -0,0 +1,124 @@
+import React, { useState } from 'react';
+
+const Home = () => {
+  //useState is a hook
+
+  const [formData,setFormData] = useState({name:"",address:"",age:""});
+  const [allInfo,setAllInfo] = useState([]);
+  const [postion,setPosition] = useState("");
+  const [showFlag,setShowFlag] = useState(false);
+
+  const onChangeHandler=(event,fieldname)=>{
+        console.log(event.target.value,">>",fieldname);
+        setFormData(prevState=>({...prevState,[fieldname]:event.target.value}));
+  }
+     
+   const onSubmitHandler =(e)=>{
+    console.log("submit call");
+    e.preventDefault();
+    setAllInfo(prevState=>[...prevState,formData]);
+    setFormData({name:"",address:"",age:""});
+   }
+const onDelete=(index)=>{
+  setAllInfo(prevState=>{
+           const filteredData = prevState.filter((item,ind)=>{
+                       if(ind !== index){
+                               return item;
+                       }
+           })
+           return filteredData;
+  })
+}
+const onEdit=(formDatafromtable,position)=>{
+  setFormData(formDatafromtable);
+  setPosition(position);
+  setShowFlag(true);
+
+  // setFormData(prevState=>({...prevState,name:formDatafromtable.name,address:formDatafromtable.address}))
+
+}
+const upSaveHandler=()=>{
+  console.log(allInfo , postion);
+  setAllInfo(prevState=>{
+    const result = prevState.map((item,index)=>{
+             if(index === postion){
+              return formData
+             }
+             else{
+              return item
+             }
+             
+    })
+    return result;
+  })
+  setFormData({name:"",address:"",age:""});
+  setShowFlag(false)
+
+}
+    return (
+      <div className='container'>
+        
+    <div className='row col-md-4 offset-md-4 mt-5'>
+          <h2 className='text-center'> Person Detail</h2>
+   
+        <form onSubmit={(e)=>onSubmitHandler(e)}>
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Name</label>
+            <input type="text" value={formData.name}  onChange={(e)=>onChangeHandler(e,"name")}  className="form-control mt-2" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter name" />
+            {/* <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small> */}
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Address</label>
+            <input type="text"  value={formData.address} onChange={(e)=>onChangeHandler(e,"address")}  className="form-control  mt-2" id="exampleInputPassword1" placeholder="address" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Age</label>
+            <input type="number"  value={formData.age} onChange={(e)=>onChangeHandler(e,"age")}   className="form-control  mt-2" id="exampleInputPassword1" placeholder="age" />
+          </div>
+       
+          {!showFlag && <button type='submit' className='btn btn-primary mt-3'>Submit</button> } 
+          {showFlag && <button type='button' onClick={()=>upSaveHandler()} className='btn btn-primary mt-3'>Save</button> } 
+        </form>
+        </div>
+
+        <div>
+             
+        </div>
+          
+<div>
+<table class="table">
+  <thead>
+    <tr>
+      
+      <th scope="col">Name</th>
+      <th scope="col">Address</th>
+      <th scope="col">Age</th>
+      <th>Edit</th>
+      <th>Delete</th>
+    </tr>
+  </thead>
+  <tbody>
+    {allInfo.map((item,index)=>{
+        return(
+          <tr>
+          <td>{item.name}</td>
+          <td>{item.address}</td>
+          <td>{item.age}</td>
+          <td><button type='button' className='btn btn-danger' onClick={()=>onEdit(item,index)}>Edit</button></td>
+          <td><button type='button' className='btn btn-secondary' onClick={()=>onDelete(index)}>Delete</button></td>
+        </tr>
+        )
+    })}
+    
+  
+   
+  </tbody>
+</table>
+</div>
+        </div>
+    );
+};
+
+export default Home;
+
+
