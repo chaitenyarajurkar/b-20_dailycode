314cb1c975e9e988749c5e6e2d8b7d32cb965dda
Chaitenya-rajurkar <rajuchetan547@gmail.com>
2023-11-18 13:56:13 +0530

    class

--- Project_ecom/src/Component/Signup/Signup.js
+++ Project_ecom/src/Component/Signup/Signup.js
@@ -1,33 +1,48 @@
 import React, { Component } from 'react';
 
 class Signup extends Component {
-        constructor(props) {
-            super(props);
-            this.state={
-                username:"shubham",
-                paasword:"",
-                errror:[],
-                formData:{}
-            }
+
+    constructor(props) {
+        super(props);
+
+        this.state={
+            name:"",
+            address:"",
+            mobileNo:["94893893893"]
         }
-        
-// ravina
+        //api will not call here
+    }
 
- functionname=()=>{
-     this.setState(prev=>({
-         ...prev,
-        username:"raviana"   
-     }))
-}
+    componentDidMount(){
+        console.log("componentDidMount")
+        this.setState({
+            name:"code first",
+            address:"pune",
+            mobileNo:[...this.state.mobileNo,"43849348394839"]
+        })
+        //api call kr skte
+    }
 
-
+    onChangeHandler(){
+        //form logic
+    
+    }
 
     render() {
+
+        const {name,address,mobileNo} = this.state;
+
         return (
             <div>
-
-                <p>{this.state.username}</p>
-                <button onClick={()=>this.functionname()}>Click here</button>
+                <h1>{name}</h1>
+                <h1>{address}</h1>
+                {/* <h1>{mobileNo}</h1> */}
+                {mobileNo.map((item)=>{
+                    return(
+                        <p>{item}</p>
+                    )
+                })}
+           
             </div>
         );
     }
