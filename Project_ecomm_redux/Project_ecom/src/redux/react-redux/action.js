d5acfb7d911822dabe86719e3f810d076090c93c
Chaitenya-rajurkar <rajuchetan547@gmail.com>
2023-12-15 21:03:17 +0530

    react-redux

--- Project_ecomm_redux/Project_ecom/src/redux/react-redux/action.js
+++ Project_ecomm_redux/Project_ecom/src/redux/react-redux/action.js
@@ -1,1 +1,45 @@
 //functiona call
+
+import { CAMERA_DATA, LAPTOP_DATA, MOBILE_DATA } from "./constant";
+
+
+
+// 1)mobile k liye
+// 2)camera k  liye 
+// 3)lptop k  liye
+
+
+const storeMobileData = (apires)=>(dispatch)=>{
+    console.log(apires);
+
+    dispatch({
+        type:MOBILE_DATA,
+        payload:{data:apires}
+    })
+
+}
+
+
+const storeCameraData = (apires)=>(dispatch)=>{
+    console.log(apires);
+
+    dispatch({
+        type:CAMERA_DATA,
+        payload:{data:apires}
+    })
+
+}
+
+const storeLaptopData = (apires)=>(dispatch)=>{
+    console.log(apires);
+
+    dispatch({
+        type:LAPTOP_DATA,
+        payload:{data:apires}
+    })
+
+}
+
+
+
+export {storeMobileData,storeCameraData,storeLaptopData}
--- Project_ecomm_redux/Project_ecom/src/redux/react-redux/reducer.js
+++ Project_ecomm_redux/Project_ecom/src/redux/react-redux/reducer.js
@@ -1,6 +1,61 @@
 // data store 
 
+import { CAMERA_DATA, LAPTOP_DATA, MOBILE_DATA } from "./constant";
+
 // obj ={
 //     mobil:"",
 //     camera:""
 // }
+
+const initialState = {
+
+    mobileData: [],
+    cameraData: [],
+    tabletData: [],
+    laptopData: [],
+    monitorData: [],
+    cartData:[],
+
+
+}
+
+// function will be create here
+// switch case logic
+//according to the switch cases data will be stored
+
+
+
+
+// action={
+//     type:"keys",
+//     payload:"cameeradata/mobiledata/laptopdata/etc..."
+// }
+
+export default function applicationReducer(state=initialState,action){
+
+    switch (action.type){
+         case MOBILE_DATA:
+             return{
+                ...state,
+                mobileData : action.payload.data
+             }
+         case CAMERA_DATA:
+            return{
+                ...state,
+                cameraData:action.payload.data
+            }
+        case LAPTOP_DATA:
+            return{
+                ...state,
+                laptopData:action.payload.data
+            }
+
+        default:
+            return{
+                ...state
+            }
+    }
+
+
+
+}
